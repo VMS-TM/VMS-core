d55aba18f7197e1485fff69843557299236d8ba0
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-06-08 16:48:41 +0300

    Revert "fix bugs"
    This reverts commit 2608a17a943fd8a3785a009e41f97c9c7ce512da.

--- src/main/java/vms/onbootloader/OnBootLoader.java
+++ src/main/java/vms/onbootloader/OnBootLoader.java
@@ -232,6 +232,14 @@ public class OnBootLoader implements ApplicationListener<ContextRefreshedEvent> 
 
 	private void loadProxyServers() {
 		ProxyServer firstProxyServer = new ProxyServer("mail@gmail.com", "D3i7&1488",
+//<<<<<<< HEAD
+//				"53c2207ef83c37171713a6e97560c819af52b44537dbc15c0451b661d5316432384e95fc24f343a88c134",
+//				"88.212.252.47", 18090);
+//
+//		ProxyServer secondProxyServer = new ProxyServer("+79858838221", "485?frR",
+//				"5d866b1300b52bd30eacd405e05a3ed90673f31f349072cf16a616ee02e8ad1a22c3eccdfe21e1f40dc88",
+//				"46.16.226.222", 53281);
+//=======
 				"53c2207ef83c37171713a6e97560c819af52b44537dbc15c0451b661d5316432384e95fc24f343a88c134",
 				"87.228.29.154", 	53281);
 
